84d0dbd29fba569a8d4b1fe3fe0be04080bc886c
Yousuf <yousufttp@gmail.com>
2021-11-15 18:25:39 +0530

    JS file for react clock

---
+++ clock/clock.js
@@ -0,0 +1,11 @@
+class Clock extends React.Component {
+    render() {
+        return (
+            <div>
+                hello there
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Clock />, document.getElementById('clock-containter'));
